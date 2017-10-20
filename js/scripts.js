c661e8c650780495bedb8fe73b9832535ac23187
Griffin P Longtin <griflongtin@live.com>
2017-10-20 14:22:35 -0700

    add some text to the form quetions

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,58 @@ $(function(){
     $("#rails-tab").hide();
     $("#c-tab").hide();
     $("#net-tab").hide();
-    $("android-tab").hide();
+    $("#android-tab").hide();
   });
+  $("button#ruby-btn").click(function(){
+    $("#home-tab").hide();
+    $("#ruby-tab").show();
+    $("#rails-tab").hide();
+    $("#c-tab").hide();
+    $("#net-tab").hide();
+    $("#android-tab").hide();
+  });
+  $("button#rails-btn").click(function(){
+    $("#home-tab").hide();
+    $("#ruby-tab").hide();
+    $("#rails-tab").show();
+    $("#c-tab").hide();
+    $("#net-tab").hide();
+    $("#android-tab").hide();
+  });
+  $("button#c-btn").click(function(){
+    $("#home-tab").hide();
+    $("#ruby-tab").hide();
+    $("#rails-tab").hide();
+    $("#c-tab").show();
+    $("#net-tab").hide();
+    $("#android-tab").hide();
+  });
+  $("button#net-btn").click(function(){
+    $("#home-tab").hide();
+    $("#ruby-tab").hide();
+    $("#rails-tab").hide();
+    $("#c-tab").hide();
+    $("#net-tab").show();
+    $("#android-tab").hide();
+  });
+  $("button#android-btn").click(function(){
+    $("#home-tab").hide();
+    $("#ruby-tab").hide();
+    $("#rails-tab").hide();
+    $("#c-tab").hide();
+    $("#net-tab").hide();
+    $("#android-tab").show();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 });
